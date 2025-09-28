820b97340760008463150e15d7524a0c221d99ef
Traman2 <tejassraman@gmail.com>
2025-09-28 01:18:10 -0500

    configured react builder transcompile app in package scripts

--- reactbuilder/src/electron/preload.ts
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-//expose a listner to the frontend, ipcRender send then called from here\
-contextBridge.exposeInMainWorld('electronAPI', {
-  closeApp: () => {
-    ipcRenderer.send('close');
-  },
-});
--- reactbuilder/src/ui/App.tsx
+++ reactbuilder/src/ui/App.tsx
@@ -1,19 +1,16 @@
 import closeButton from './icons/closeButton.svg';
 
 function App() {
-  const handleClose = () => {
-    window.electronAPI.closeApp();
-  };
 
   return (
     <>
       <div className="flex flex-col h-40 w-200 bg-black/80 rounded-lg backdrop-blur-3xl">
-      <nav className="flex justify-between px-4 py-1">
+      <nav className="flex justify-between px-4 py-3">
         <div>
           <p className="text-white font-bold">Assignment Cheater 2.0</p>
         </div>
         <div>
-          <button onClick={handleClose} className="hover:opacity-45 transition-all cursor-pointer"><img src={closeButton} className="w-5"/></button>
+          <button className="hover:opacity-45 transition-all cursor-pointer"><img src={closeButton} className="w-5"/></button>
         </div>
       </nav>
       <div className="flex mt-2">
